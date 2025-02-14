7e2d6b899b91744403fd0db2ca167d9271b49710
Murilo Ribeiro <160795915+omuriloribeiro@users.noreply.github.com>
2025-02-14 00:41:40 +0000

    initial commit

---
+++ exercicio-01/app/page.js
@@ -0,0 +1,18 @@
+function Texto1() {
+  return <p>OLA OLA OLA</p>;
+}
+
+export default function Home() {
+  const nome = "eduardo";
+  return (
+  <div>
+    <h1>Mano {nome} é o cara</h1>
+    <p>el mano</p>
+    <button>denis dj</button>
+    <Texto1></Texto1>
+    <Texto1></Texto1>
+  </div>
+ 
+
+  );
+}
