276e11890062ad2926e8bad1d0ca5ab58f5910fd
Murilo Ribeiro <160795915+omuriloribeiro@users.noreply.github.com>
2025-03-14 00:31:55 +0000

    att

--- exercicio-01/app/dados/com/dado.js
+++ exercicio-01/app/dados/com/dado.js
@@ -1,11 +1,11 @@
 export function Dado({ valor }){
     const imagens = {
-      1: "dado_1.jpeg",
-      2: "dado_2.jpeg",
-      3: "dado_3.jpeg",
-      4: "dado_4.jpeg",
-      5: "dado_5.jpeg",
-      6: "dado_6.jpeg",
+      1: "dado_1.png",
+      2: "dado_2.png",
+      3: "dado_3.png",
+      4: "dado_4.png",
+      5: "dado_5.png",
+      6: "dado_6.png",
     };
   
     return (
